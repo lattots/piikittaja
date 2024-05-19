857daaeb33228cf1b10481a55886c4bb67f5ac46
otso <otso@lattu.biz>
2024-05-19 10:23:44 +0300

    New web UI for Piikki...
    Added web UI for Piikki. This makes it possible to handle transactions as an "admin" from the web UI. UI is designed to be used only by admins as you can pay back user tabs from the UI.

---
+++ src/web_server/server.go
@@ -0,0 +1,198 @@
+package main
+
+import (
+	"database/sql"
+	"fmt"
+	_ "github.com/go-sql-driver/mysql"
+	"github.com/joho/godotenv"
+	"github.com/lattots/piikittaja/src/user"
+	"html/template"
+	"log"
+	"net/http"
+	"os"
+	"strconv"
+)
+
+func main() {
+	err := godotenv.Load("../../data/.env")
+	if err != nil {
+		log.Fatalln("error loading .env file: ", err)
+	}
+
+	router := http.NewServeMux()
+
+	staticFileHandler := http.FileServer(http.Dir("."))
+	router.Handle("/css/", staticFileHandler)
+	router.HandleFunc("/", handleIndex)
+	router.HandleFunc("/action", handleUserAction)
+	router.HandleFunc("/user-view", handleUserView)
+
+	port := ":3000"
+	fmt.Printf("Server started on port %s\n", port)
+	if err := http.ListenAndServe(port, router); err != nil {
+		log.Fatalln("unexpected error: ", err)
+	}
+}
+
+func handleIndex(w http.ResponseWriter, r *http.Request) {
+	indexTempl, err := template.ParseFiles("./html/index.html")
+	if err != nil {
+		log.Println("error parsing html template")
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	// Define a template with the name "index" to ensure only that block is executed
+	tmpl := indexTempl.Lookup("index")
+	if tmpl == nil {
+		log.Println("error finding index block in template")
+		http.Error(w, "index block not found in template", http.StatusInternalServerError)
+		return
+	}
+	if err := tmpl.Execute(w, nil); err != nil {
+		log.Println("error executing html template")
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+}
+
+type Action struct {
+	Username string
+	Action   string
+	Amount   int
+}
+
+func handleUserAction(w http.ResponseWriter, r *http.Request) {
+	tmpl, err := template.ParseFiles("./html/index.html")
+	if err != nil {
+		log.Println("error parsing html template")
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	actionForm := tmpl.Lookup("action-form")
+	if actionForm == nil {
+		log.Println("error finding action-form block in template")
+		http.Error(w, "action-form block not found in template", http.StatusInternalServerError)
+		return
+	}
+
+	if err := r.ParseForm(); err != nil {
+		log.Println("error parsing form")
+	}
+
+	var actionStatus string
+
+	amount, err := strconv.Atoi(r.FormValue("amount"))
+	if err != nil {
+		log.Println("error converting amount to integer")
+		actionStatus = "Jokin meni pieleen..."
+	} else {
+		actionStatus = "Onnistui!"
+	}
+	action := Action{
+		Username: r.FormValue("username"),
+		Action:   r.FormValue("action-type"),
+		Amount:   amount,
+	}
+
+	// Database URL is read from environment variables.
+	dbURL := os.Getenv("DATABASE_APP")
+	if dbURL == "" {
+		log.Println("error getting database URL", err)
+		http.Error(w, "error fetching users", http.StatusInternalServerError)
+		return
+	}
+
+	// Database handle is created for user.
+	db, err := sql.Open("mysql", dbURL)
+	if err != nil {
+		log.Println("error connecting to database: ", err)
+		http.Error(w, "error fetching user", http.StatusInternalServerError)
+		return
+	}
+
+	defer db.Close()
+
+	usr, err := user.GetUserByUsername(db, action.Username)
+	if err != nil {
+		log.Println("error fetching user from database", err)
+		http.Error(w, "error fetching user", http.StatusInternalServerError)
+		return
+	}
+
+	if action.Action == "borrow" {
+		err = usr.AddToTab(action.Amount)
+	} else if action.Action == "pay" {
+		err = usr.PayBackTab(action.Amount)
+	} else {
+		log.Println("unknown action", err)
+		http.Error(w, "unknown action", http.StatusBadRequest)
+		return
+	}
+	if err != nil {
+		log.Println("error adding to users tab", err)
+		http.Error(w, "error adding to users tab", http.StatusInternalServerError)
+		return
+	}
+
+	// Create a map to hold the status message
+	data := map[string]interface{}{
+		"ActionStatus": actionStatus,
+	}
+
+	if err := actionForm.Execute(w, data); err != nil {
+		log.Println("error executing html template")
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+}
+
+func handleUserView(w http.ResponseWriter, r *http.Request) {
+	tmpl, err := template.ParseFiles("./html/index.html")
+	if err != nil {
+		log.Println("error parsing html template")
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	singleUser := tmpl.Lookup("single-user-view")
+	if singleUser == nil {
+		log.Println("error finding single-user-view block in template")
+		http.Error(w, "single-user-view block not found in template", http.StatusInternalServerError)
+		return
+	}
+
+	// Database URL is read from environment variables.
+	dbURL := os.Getenv("DATABASE_APP")
+	if dbURL == "" {
+		log.Println("error getting database URL", err)
+		http.Error(w, "error fetching users", http.StatusInternalServerError)
+		return
+	}
+
+	// Database handle is created for user.
+	db, err := sql.Open("mysql", dbURL)
+	if err != nil {
+		log.Println("error connecting to database: ", err)
+		http.Error(w, "error fetching users", http.StatusInternalServerError)
+		return
+	}
+
+	defer db.Close()
+
+	users, err := user.GetUsers(db)
+	if err != nil {
+		log.Println("error fetching users from db", err)
+		http.Error(w, "error fetching users", http.StatusInternalServerError)
+		return
+	}
+
+	for _, u := range users {
+		err := singleUser.Execute(w, u)
+		if err != nil {
+			log.Println("error executing single user template")
+			http.Error(w, "could not execute single user template", http.StatusInternalServerError)
+			return
+		}
+	}
+}
