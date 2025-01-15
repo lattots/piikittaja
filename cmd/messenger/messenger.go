3faf58bf4f661f51aedac9d63f6d89d3b050fba9
lattots <otso@lattu.biz>
2025-01-15 11:42:25 +0200

    Update user facing instructions + Messenger...
    - Due to changes in the PiikkiBotti business logic, the user instructions are updated to reflect the changes
    - Added user messenger app to send user base wide messages

---
+++ cmd/messenger/messenger.go
@@ -0,0 +1,63 @@
+package main
+
+import (
+	"database/sql"
+	"errors"
+	"fmt"
+	"log"
+	"os"
+
+	_ "github.com/go-sql-driver/mysql"
+	"github.com/go-telegram/bot"
+	"github.com/joho/godotenv"
+
+	"github.com/lattots/piikittaja/pkg/user"
+)
+
+func main() {
+	err := godotenv.Load("./assets/.env")
+	if err != nil {
+		log.Fatalln("error loading environment variables: ", err)
+	}
+
+	// Database URL is read from environment variables.
+	dbURL := os.Getenv("DATABASE_APP")
+	if dbURL == "" {
+		log.Fatalln("error getting database URL from environment variables")
+	}
+
+	// Database handle is created.
+	db, err := sql.Open("mysql", dbURL)
+	if err != nil {
+		log.Fatalln("error opening database connection:", err)
+	}
+
+	fmt.Println("Creating bot...")
+	b, err := bot.New(os.Getenv("TELEGRAM_BOT_TOKEN"))
+	if err != nil {
+		log.Fatalln("error creating bot:\n", err)
+	}
+
+	users, err := user.GetUsers(db)
+	if err != nil {
+		log.Fatalln("error fetching users:", err)
+	}
+
+	for _, u := range users {
+		log.Printf("Sending message to %s...\n", u.Username)
+		msg := fmt.Sprintf(
+			"MUUTOKSIA PIIKKIBOTIN TOIMINTAAN - LUE TÄMÄ!!\n\n" +
+				"Koska joihinkin teistä ronteista ei voi luottaa velkojen maksussa, PiikkiBotti toimii jatkossa " +
+				"pankkitilin tavoin. Tämä tarkoittaa siis sitä, että ilmaisia lounaita ei enää ole, ja tilin saldoa " +
+				"tulee kerryttää etukäteen. PiikkiBotti ei siis enää myönnä lainaa.\n\n" +
+				"PiikkiBottia kannattaa kuitenkin edelleen käyttää, sillä käteisen kantaminen ns. \"sucks ass\".",
+		)
+		err := u.SendMessage(b, msg)
+		if errors.Is(bot.ErrorForbidden, err) {
+			log.Printf("User: %s has probably blocked PiikkiBotti...\nError: %s\n", u.Username, err)
+		} else if err != nil {
+			log.Printf("fatal error while sending message to %s: %s\n", u.Username, err)
+		}
+	}
+	fmt.Println("Payment reminders sent!")
+}
