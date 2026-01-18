56a6fda30b4dcbb01a00c8bcc2be2177bb473753
lattots <otso@lattu.biz>
2026-01-18 11:54:55 +0200

    FEAT: Added more test data

--- db/create_test_data.sql
+++ db/create_test_data.sql
@@ -1,3 +1,49 @@
 INSERT INTO users (id, username, balance) VALUES (123, 'Kanni Kala', 6900); -- 69 €
 INSERT INTO users (id, username, balance) VALUES (456, 'Humalainen Haukka', 42000); -- 420 €
 INSERT INTO users (id, username, balance) VALUES (789, 'Vodka Kotka', 6700); -- 67 €
+INSERT INTO users (id, username, balance) VALUES (101, 'Sisu Karhu', 15000); -- 150 €
+INSERT INTO users (id, username, balance) VALUES (102, 'Lumi Peura', 8500); -- 85 €
+INSERT INTO users (id, username, balance) VALUES (103, 'Tunturi Susi', 2500); -- 25 €
+INSERT INTO users (id, username, balance) VALUES (106, 'Pouta Pilvi', -500); -- -5 €
+INSERT INTO users (id, username, balance) VALUES (108, 'Kulta Kettu', 11000); -- 110 €
+INSERT INTO users (id, username, balance) VALUES (109, 'Halla Hirvi', 4500); -- 45 €
+INSERT INTO users (id, username, balance) VALUES (110, 'Aamun Virkku', 900); -- 9 €
+
+INSERT INTO transactions (userId, adminEmail, amount) VALUES (123, "test.adming@gmail.com", -200);
+INSERT INTO transactions (userId, adminEmail, amount) VALUES (123, "test.adming@gmail.com", 200);
+
+INSERT INTO transactions (userId, adminEmail, amount) VALUES (456, "test.adming@gmail.com", -37800);
+
+INSERT INTO transactions (userId, adminEmail, amount) VALUES (789, "test.adming@gmail.com", 200);
+
+INSERT INTO transactions (userId, adminEmail, amount) VALUES (101, "test.admin@gmail.com", -2000);
+INSERT INTO transactions (userId, adminEmail, amount) VALUES (101, "test.admin@gmail.com", -1500);
+INSERT INTO transactions (userId, adminEmail, amount) VALUES (101, "test.admin@gmail.com", 5000);
+INSERT INTO transactions (userId, adminEmail, amount) VALUES (101, "test.admin@gmail.com", -300);
+INSERT INTO transactions (userId, adminEmail, amount) VALUES (101, "test.admin@gmail.com", -1200);
+INSERT INTO transactions (userId, adminEmail, amount) VALUES (101, "test.admin@gmail.com", 5000);
+INSERT INTO transactions (userId, adminEmail, amount) VALUES (101, "test.admin@gmail.com", -300);
+INSERT INTO transactions (userId, adminEmail, amount) VALUES (101, "test.admin@gmail.com", -1200);
+INSERT INTO transactions (userId, adminEmail, amount) VALUES (101, "test.admin@gmail.com", 5000);
+INSERT INTO transactions (userId, adminEmail, amount) VALUES (101, "test.admin@gmail.com", -300);
+INSERT INTO transactions (userId, adminEmail, amount) VALUES (101, "test.admin@gmail.com", -1200);
+INSERT INTO transactions (userId, adminEmail, amount) VALUES (101, "test.admin@gmail.com", 5000);
+INSERT INTO transactions (userId, adminEmail, amount) VALUES (101, "test.admin@gmail.com", -300);
+INSERT INTO transactions (userId, adminEmail, amount) VALUES (101, "test.admin@gmail.com", -1200);
+INSERT INTO transactions (userId, adminEmail, amount) VALUES (101, "test.admin@gmail.com", 5000);
+INSERT INTO transactions (userId, adminEmail, amount) VALUES (101, "test.admin@gmail.com", -300);
+INSERT INTO transactions (userId, adminEmail, amount) VALUES (101, "test.admin@gmail.com", -1200);
+
+INSERT INTO transactions (userId, adminEmail, amount) VALUES (103, "test.admin@gmail.com", -1000);
+INSERT INTO transactions (userId, adminEmail, amount) VALUES (103, "test.admin@gmail.com", -1500);
+INSERT INTO transactions (userId, adminEmail, amount) VALUES (103, "test.admin@gmail.com", -500);
+INSERT INTO transactions (userId, adminEmail, amount) VALUES (103, "test.admin@gmail.com", 2000);
+
+INSERT INTO transactions (userId, adminEmail, amount) VALUES (106, "test.admin@gmail.com", 1000);
+INSERT INTO transactions (userId, adminEmail, amount) VALUES (106, "test.admin@gmail.com", -800);
+
+INSERT INTO transactions (userId, adminEmail, amount) VALUES (108, "test.admin@gmail.com", -5000);
+INSERT INTO transactions (userId, adminEmail, amount) VALUES (108, "test.admin@gmail.com", -2000);
+INSERT INTO transactions (userId, adminEmail, amount) VALUES (108, "test.admin@gmail.com", 4000);
+INSERT INTO transactions (userId, adminEmail, amount) VALUES (108, "test.admin@gmail.com", -1000);
+INSERT INTO transactions (userId, adminEmail, amount) VALUES (108, "test.admin@gmail.com", -500);
