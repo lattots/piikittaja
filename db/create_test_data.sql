a050a7086a4d29c6ff2a049caa4467757bea6372
lattots <otso@lattu.biz>
2026-01-19 21:05:16 +0200

    FIX: Add more dummy data to development database

--- db/create_test_data.sql
+++ db/create_test_data.sql
@@ -9,9 +9,10 @@ INSERT INTO transactions (userId, adminEmail, amount) VALUES (123, "test.adming@
 INSERT INTO transactions (userId, adminEmail, amount) VALUES (456, "test.adming@gmail.com", -37800);
 
 INSERT INTO transactions (userId, adminEmail, amount) VALUES (789, "test.adming@gmail.com", 200);
+INSERT INTO transactions (userId, adminEmail, amount) VALUES (789, "test.adming@gmail.com", -20);
 
 INSERT INTO transactions (userId, adminEmail, amount) VALUES (101, "test.admin@gmail.com", -2000);
-INSERT INTO transactions (userId, adminEmail, amount) VALUES (101, "test.admin@gmail.com", -1500);
+INSERT INTO transactions (userId, adminEmail, amount) VALUES (101, "test.admin@gmail.com", -1501);
 INSERT INTO transactions (userId, adminEmail, amount) VALUES (101, "test.admin@gmail.com", 5000);
 INSERT INTO transactions (userId, adminEmail, amount) VALUES (101, "test.admin@gmail.com", -300);
 INSERT INTO transactions (userId, adminEmail, amount) VALUES (101, "test.admin@gmail.com", -1200);
@@ -27,3 +28,52 @@ INSERT INTO transactions (userId, adminEmail, amount) VALUES (101, "test.admin@g
 INSERT INTO transactions (userId, adminEmail, amount) VALUES (101, "test.admin@gmail.com", 5000);
 INSERT INTO transactions (userId, adminEmail, amount) VALUES (101, "test.admin@gmail.com", -300);
 INSERT INTO transactions (userId, adminEmail, amount) VALUES (101, "test.admin@gmail.com", -1200);
+
+INSERT INTO users (id, username, firstName, lastName, balance) VALUES (1001, 'gambina_galgo', 'Gambina', 'Galgo', 1250); -- 12.50 €
+INSERT INTO users (id, username, firstName, lastName, balance) VALUES (1002, 'rommi_rotta', 'Rommi', 'Rotta', -500); -- -5.00 €
+INSERT INTO users (id, username, firstName, lastName, balance) VALUES (1003, 'siideri_siili', 'Siideri', 'Siili', 2200); -- 22.00 €
+INSERT INTO users (id, username, firstName, lastName, balance) VALUES (1004, 'kossu_koira', 'Kossu', 'Koira', 4500); -- 45.00 €
+INSERT INTO users (id, username, firstName, lastName, balance) VALUES (1005, 'viini_vuohi', 'Viini', 'Vuohi', 0); -- 0.00 €
+INSERT INTO users (id, username, firstName, lastName, balance) VALUES (1006, 'olut_orava', 'Olut', 'Orava', 890); -- 8.90 €
+INSERT INTO users (id, username, firstName, lastName, balance) VALUES (1007, 'skumppa_sammakko', 'Skumppa', 'Sammakko', 15000); -- 150.00 €
+INSERT INTO users (id, username, firstName, lastName, balance) VALUES (1008, 'gin_gepardi', 'Gin', 'Gepardi', 330); -- 3.30 €
+INSERT INTO users (id, username, firstName, lastName, balance) VALUES (1009, 'ponu_poro', 'Ponu', 'Poro', -1200); -- -12.00 €
+INSERT INTO users (id, username, firstName, lastName, balance) VALUES (1012, 'limu_lisko', 'Limu', 'Lisko', 120); -- 1.20 €
+INSERT INTO users (id, username, firstName, lastName, balance) VALUES (1013, 'viski_villisika', 'Viski', 'Villisika', 25000); -- 250.00 €
+INSERT INTO users (id, username, firstName, lastName, balance) VALUES (1014, 'pontikka_pupu', 'Pontikka', 'Pupu', -450); -- -4.50 €
+INSERT INTO users (id, username, firstName, lastName, balance) VALUES (1015, 'kalja_kana', 'Kalja', 'Kana', 1800); -- 18.00 €
+INSERT INTO users (id, username, firstName, lastName, balance) VALUES (1017, 'valkoviini_varis', 'Valkoviini', 'Varis', 420); -- 4.20 €
+INSERT INTO users (id, username, firstName, lastName, balance) VALUES (1018, 'shotti_susi', 'Shotti', 'Susi', -3000); -- -30.00 €
+INSERT INTO users (id, username, firstName, lastName, balance) VALUES (1019, 'kilju_kiuru', 'Kilju', 'Kiuru', 150); -- 1.50 €
+INSERT INTO users (id, username, firstName, lastName, balance) VALUES (1020, 'absintti_ahma', 'Absintti', 'Ahma', 7700); -- 77.00 €
+INSERT INTO users (id, username, firstName, lastName, balance) VALUES (1021, 'brandy_biisoni', 'Brandy', 'Biisoni', 1200); -- 12.00 €
+INSERT INTO users (id, username, firstName, lastName, balance) VALUES (1022, 'tonic_tiikeri', 'Tonic', 'Tiikeri', 250); -- 2.50 €
+INSERT INTO users (id, username, firstName, lastName, balance) VALUES (1023, 'konjakki_kolibri', 'Konjakki', 'Kolibri', 30000); -- 300.00 €
+INSERT INTO users (id, username, firstName, lastName, balance) VALUES (1024, 'tequila_turska', 'Tequila', 'Turska', -100); -- -1.00 €
+INSERT INTO users (id, username, firstName, lastName, balance) VALUES (1025, 'portteri_pulla', 'Portteri', 'Pulla', 550); -- 5.50 €
+INSERT INTO users (id, username, firstName, lastName, balance) VALUES (1026, 'stout_stauri', 'Stout', 'Stauri', 440); -- 4.40 €
+INSERT INTO users (id, username, firstName, lastName, balance) VALUES (1027, 'ale_ankka', 'Ale', 'Ankka', 2100); -- 21.00 €
+INSERT INTO users (id, username, firstName, lastName, balance) VALUES (1028, 'pilsneri_pingviini', 'Pilsneri', 'Pingviini', 990); -- 9.90 €
+INSERT INTO users (id, username, firstName, lastName, balance) VALUES (1029, 'lager_lammas', 'Lager', 'Lammas', 1350); -- 13.50 €
+INSERT INTO users (id, username, firstName, lastName, balance) VALUES (1030, 'spritz_saukko', 'Spritz', 'Saukko', 6000); -- 60.00 €
+
+INSERT INTO transactions (userId, adminEmail, amount) VALUES (1001, "test.admin@gmail.com", 2000);
+INSERT INTO transactions (userId, adminEmail, amount) VALUES (1001, "test.admin@gmail.com", -750);
+INSERT INTO transactions (userId, adminEmail, amount) VALUES (1002, "test.admin@gmail.com", -500);
+INSERT INTO transactions (userId, adminEmail, amount) VALUES (1003, "test.admin@gmail.com", 3000);
+INSERT INTO transactions (userId, adminEmail, amount) VALUES (1003, "test.admin@gmail.com", -800);
+INSERT INTO transactions (userId, adminEmail, amount) VALUES (1004, "test.admin@gmail.com", 5000);
+INSERT INTO transactions (userId, adminEmail, amount) VALUES (1004, "test.admin@gmail.com", -500);
+INSERT INTO transactions (userId, adminEmail, amount) VALUES (1007, "test.admin@gmail.com", 20000);
+INSERT INTO transactions (userId, adminEmail, amount) VALUES (1007, "test.admin@gmail.com", -5000);
+INSERT INTO transactions (userId, adminEmail, amount) VALUES (1009, "test.admin@gmail.com", -1200);
+INSERT INTO transactions (userId, adminEmail, amount) VALUES (1013, "test.admin@gmail.com", 30000);
+INSERT INTO transactions (userId, adminEmail, amount) VALUES (1013, "test.admin@gmail.com", -5000);
+INSERT INTO transactions (userId, adminEmail, amount) VALUES (1018, "test.admin@gmail.com", -1000);
+INSERT INTO transactions (userId, adminEmail, amount) VALUES (1018, "test.admin@gmail.com", -2000);
+INSERT INTO transactions (userId, adminEmail, amount) VALUES (1020, "test.admin@gmail.com", 10000);
+INSERT INTO transactions (userId, adminEmail, amount) VALUES (1020, "test.admin@gmail.com", -2300);
+INSERT INTO transactions (userId, adminEmail, amount) VALUES (1023, "test.admin@gmail.com", 35000);
+INSERT INTO transactions (userId, adminEmail, amount) VALUES (1023, "test.admin@gmail.com", -5000);
+INSERT INTO transactions (userId, adminEmail, amount) VALUES (1030, "test.admin@gmail.com", 7000);
+INSERT INTO transactions (userId, adminEmail, amount) VALUES (1030, "test.admin@gmail.com", -1000);
