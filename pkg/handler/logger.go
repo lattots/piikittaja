99227707e7948425694465664baf0a5ddc6803c4
lattots <otso@lattu.biz>
2026-03-07 19:00:57 +0200

    REFACTOR: Better request logging in the backend

--- pkg/handler/logger.go
+++ pkg/handler/logger.go
@@ -24,11 +24,39 @@ func Log(handlerFunc http.HandlerFunc) http.HandlerFunc {
 
 		handlerFunc(rec, r)
 
-		log.Printf("%s -> %d - %s: Response took %d ms\n",
+		duration := time.Since(start).Milliseconds()
+		statusColor := getStatusColor(rec.statusCode)
+
+		log.Printf("%s%s%s %s -> %s%d - %s%s %stook %d ms%s\n",
+			grey, r.Method, reset,
 			r.URL.Path,
-			rec.statusCode,
-			http.StatusText(rec.statusCode),
-			time.Since(start).Milliseconds(),
+			statusColor, rec.statusCode,
+			http.StatusText(rec.statusCode), reset,
+			grey, duration, reset,
 		)
+	}
+}
+
+const (
+	reset  = "\033[0m"
+	red    = "\033[31m"
+	green  = "\033[32m"
+	yellow = "\033[33m"
+	blue   = "\033[34m"
+	grey   = "\033[90m"
+)
+
+func getStatusColor(code int) string {
+	switch {
+	case code >= 500:
+		return red
+	case code >= 400:
+		return yellow
+	case code >= 300:
+		return blue
+	case code >= 200:
+		return green
+	default:
+		return reset
 	}
 }
